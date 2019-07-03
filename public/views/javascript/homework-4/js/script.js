af6589b70b0b0181a4502c77183c2c22b30a016a
Krokusmas <anton.sedchenko@gmail.com>
2019-07-03 14:10:21 +0300

    HW4

--- public/views/javascript/homework-4/js/script.js
+++ public/views/javascript/homework-4/js/script.js
@@ -15,117 +15,117 @@
 // От 0 до 4 в обоих случаях т.к. инструкция alert будет выполняться сразу если условие верно и только после этого переменная будет увеличена в обоих случаях.
 
 // Task 4
-var i;
-for (i = 2; i <= 10; i += 1) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-}
+// var i;
+// for (i = 2; i <= 10; i += 1) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
 
 // Task 5
-var i = 0;
-while (i < 3) {
-  console.log('номер ' + i + '!');
-  i + 1;
-}
+// var i = 0;
+// while (i < 3) {
+//   console.log('номер ' + i + '!');
+//   i += 1;
+// }
 
 // Task 6
-var num;
-while (true) {
-  num = +prompt('Введите число больше 100', '');
-  if (num > 100) {
-    break;
-  } else if (!num) {
-    break;
-  }
-} 
+// var num;
+// while (true) {
+//   num = +prompt('Введите число больше 100', '');
+//   if (num > 100) {
+//     break;
+//   } else if (!num) {
+//     break;
+//   }
+// } 
 
 // Второй вариант через do..while
-var num;
-do {
-  num = +prompt('Введите число больше 100', '');
-} while (i <= 100 && num != null);
+// var num;
+// do {
+//   num = prompt('Введите число больше 100', '');
+// } while (num <= 100 && num !== null);
 
 // Task 7
-nextPrime:
-for (var i = 2; i <= 10; i += 1) {
-  for (var j = 2; j < i; j += 1) {
-    if (i % j === 0) continue nextPrime 
-  }
-  console.log(i); 
-}
+// nextPrime:
+// for (var i = 2; i <= 10; i += 1) {
+//   for (var j = 2; j < i; j += 1) {
+//     if (i % j === 0) continue nextPrime 
+//   }
+//   console.log(i); 
+// }
 
 // Unit link https://learn.javascript.ru/switch
 
 // Task 1
-var browser = 'Chrome';
-if (browser === 'IE') {
-  console.log('О, да у вас IE!');
-} else if (browser === 'Chrome'|| browser === 'Firefox' || browser === 'Safari' || browser === 'Opera'); {
-    console.log('Да, и эти браузеры мы поддерживаем');
-  } else { 
-    console.log('Мы надеемся, что и в вашем браузере все ок!');
-  }
+// var browser = 'Chrome';
+// if (browser === 'IE') {
+//   console.log('О, да у вас IE!');
+// } else if (browser === 'Chrome'|| browser === 'Firefox' || browser === 'Safari' || browser === 'Opera'); {
+//   console.log('Да, и эти браузеры мы поддерживаем');
+// } else { 
+//   console.log('Мы надеемся, что и в вашем браузере все ок!');
+// }
 
 // Task 2
-var a = +prompt('a?', '');
-switch (a) {
-  case 0: {
-    console.log(0);
-    break;
-  } 
-  case 1: {
-    console.log(1);
-    break; 
-  }
-  case 2:
-  case 3: {
-    console.log('2,3');
-    break;
-  }
-}
+// var a = +prompt('a?', '');
+// switch (a) {
+//   case 0: {
+//     console.log(0);
+//     break;
+//   } 
+//   case 1: {
+//     console.log(1);
+//     break; 
+//   }
+//   case 2:
+//   case 3: {
+//     console.log('2,3');
+//     break;
+//   }
+// }
 
 // Unit link https://learn.javascript.ru/function-basics
 
 // Task 1
-// Есть else или нет - без разницы, программа работает одинаково.
+// Есть else или нет - без разницы, программа работает одинаково. В варианте без else проверяется условие цикла, если оно не верно, то не указано, что делать, код просто выполняется дальше и получает команду вывести сообщение с подтверждением. Также сам по себе if не подразумевает прерывания цикла при невыполнении своего условия.  
 
 // Task 2
 // Вариант с '?'
-function checkAge (age) {
-  return (age > 18) ? true : confirm('Родители разрешили?');
-}
+// function checkAge (age) {
+//   return (age > 18) ? true : confirm('Родители разрешили?');
+// }
 
-// Вариант с ||
-function checkAge (age) {
-  return (age > 18) || confirm('Родители разрешили?');
-}
+// // Вариант с ||
+// function checkAge (age) {
+//   return (age > 18) || confirm('Родители разрешили?');
+// }
 
 // Task 3
-function min (a, b) {
-  if (a < b) {
-    return a;
-  }
-  return b;
-}
-var a = +prompt('Введите а', '');
-var b = +prompt('Введите b', '');
-min(a, b);
+// function min (a, b) {
+//   if (a < b) {
+//     return a;
+//   }
+//   return b;
+// }
+// var a = +prompt('Введите а', '');
+// var b = +prompt('Введите b', '');
+// min(a, b);
 
 // Task 4
-function pow (x, n) {
-  var result = x;
-  var i;
-  for (i = 1; i < n; i += 1) {
-    result *= x;
-  }
-  return result;
-}
-var x = +prompt('Введите число', '');
-var n = +prompt('Введите степень', '');
+// function pow (x, n) {
+//   var result = x;
+//   var i;
+//   for (i = 1; i < n; i += 1) {
+//     result *= x;
+//   }
+//   return result;
+// }
+// var x = +prompt('Введите число', '');
+// var n = +prompt('Введите степень', '');
 
-if (n < 1) {
-  console.log('Неверный формат. Введите число > 1');
-} else {
-  pow (x, n);
-}
+// if (n < 1) {
+//   console.log('Неверный формат. Введите число > 1');
+// } else {
+//   pow (x, n);
+// }
