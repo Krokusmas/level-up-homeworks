3c68885a90e9b8e43df903adcc948d6bba869a88
Krokusmas <anton.sedchenko@gmail.com>
2019-07-12 17:11:32 +0300

    HW7

--- public/views/javascript/homework-7/js/script.js
+++ public/views/javascript/homework-7/js/script.js
@@ -33,7 +33,7 @@
 // var salaries = {
 //   "Вася": 100,
 //   "Петя": 300,
-//   "Даша": 250
+//   "Даша": 250,
 // };
 
 // function salariesSum (obj) {
@@ -47,35 +47,119 @@
 // console.log(salariesSum(salaries));
 
 // Task 3
-// Есть объект salaries с зарплатами. Напишите код, который выведет имя сотрудника, у которого самая большая зарплата.
+// var salaries = {
+//   "Вася": 100,
+//   "Петя": 300,
+//   "Даша": 250,
+// };
 
-// Если объект пустой, то пусть он выводит «нет сотрудников».
+// var counter = 0;
+// var maxSalary = 0;
+// var worker;
+// var key;
 
-// Например:
+// for (key in salaries) {
+//   counter += 1;
+//   if (maxSalary < salaries[key]) {
+//     maxSalary = salaries[key];
+//     worker = key;
+//   }
+// }
+// if (counter < 1) {
+//   console.log('нет сотрудников');
+// } else {
+//   console.log(worker);
+// }
 
-var salaries = {
-  "Вася": 100,
-  "Петя": 300,
-  "Даша": 250
-};
-// ... ваш код выведет "Петя"
-var counter = 0;
-var salary = 0;
-var worker;
-var key;
+// Task 4
+// var menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu",
+// };
 
-for (key in salaries) {
-  counter += 1;
-  if (salary < salaries[key]) {
-    worker = [key];
-  } 
-  if (salary >= salaries[key]) {
-      break;
+// function isNumeric (n) {
+//   return !isNaN(parseFloat(n)) && isFinite(n)
+// }
+
+// var key;
+// var counter = 0;
+
+// function multiplyNumeric (n) {
+//   for (key in menu) {
+//     if (isNumeric(menu[key])) {
+//       menu[key] *= 2;
+//     } 
+//   }
+//   return menu;
+// }
+// console.log(menu);
+// console.log(multiplyNumeric(menu));
+
+// ADDITIONAL TASKS
+// Reverse the given string
+// (function () {
+//   function funcName (param) {
+//     // code...
+//   }
+//   funcName('Some string'); // -> 'gnirts emoS'
+// }());
+
+var result = '';
+var i;
+
+  function strReverse (str) {
+    let n = str.length;
+    for (i = 0; i < str.length; i += 1) {
+      result += str[n];
+      n -= 1; 
+    }
+    return result;
   }
-  if (counter < 1) {
-    console.log('нет сотрудников');
-  }
-}
-console.log(worker);
+  strReverse('Some string');
 
 
+
+// Compare two objects
+// This function must return true if two objects refer to the same object or they refer to different objects but have the same keys number and their corresponding values are equal. The function must work with unlimited nestend objects. You can use "book" object with different set of properties to test.
+// (function () {
+//   function funcName (param1, param2) {
+//     // code...
+//   }
+//   funcName(
+//     { key: 'Value' },
+//     { key: 'Value' }
+//   ); // -> true
+//   funcName(
+//     { key: 'Value' },
+//     { key: 'Value 1' }
+//   ); // -> false
+//   funcName(
+//     { key: 'Value', nested: { key: 'Value' } },
+//     { key: 'Value', nested: { key: 'Value' } }
+//   ); // -> true
+//   funcName(
+//     { key: 'Value', nested: { key: 'Value' } },
+//     { key: 'Value 1', nested: { key: 'Value' } }
+//   ); // -> false
+// }());
+
+// // Draw an area
+// // Draw an area N wide and N height like it the example below. Use ' @ ' and ' - '.
+// (function () {
+//   function funcName (N) {
+//     // code...
+//   }
+//   funcName(8); // -> http://prntscr.com/jpobhn
+// }());
+
+// // Strings comparison
+// // Check whether two strings have the same set of chars. Return true if they are equal length and have the same set of chars no matter at which index those chars are
+// (function () {
+//   function funcName (param1, param2) {
+//     // code...
+//   }
+//   funcName('clock', 'lockc'); // -> true. Because two "c", one "l", one "o" and one "k". Doesn't matter they are at different positions
+//   funcName('tree', 'three'); // -> false. Different lenghts
+//   funcName('mttudd', 'dutmtd'); // -> true. Because two "t", two "d", one "m" and one "u". Doesn't matter they are at different positions
+// }());
