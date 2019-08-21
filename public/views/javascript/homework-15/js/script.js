ce2ada8f518be0cf590a1b7787fca504eb60e17e
Krokusmas <anton.sedchenko@gmail.com>
2019-08-21 19:23:42 +0300

    HW 15/16

--- public/views/javascript/homework-15/js/script.js
+++ public/views/javascript/homework-15/js/script.js
@@ -18,30 +18,62 @@
 
 // Task 5 не решил
 
-// Task 6 не понял как работает выделеный участок кода, который надо исправить.
+// Task 6 не решил
 
 // Unit link - http://web.archive.org/web/20190405104501/https://learn.javascript.ru/decorator
 
 // Task 1
-function work(a) {
-  let result = 0;
-  return result += a;
-}
+// function work(a) {
+//   let result = 0;
+//   return result += a;
+// }
 
-function makeLogging(f, log) {
-  return function (a) {
-    let result = f.apply(this, arguments);
-    log.push(a);
-    return result;
-  }
-}
+// function makeLogging(f, log) {
+//   return function (a) {
+//     let result = f.apply(this, arguments);
+//     log.push(a);
+//     return result;
+//   }
+// }
 
-var log = [];
-work = makeLogging(work, log);
+// var log = [];
+// work = makeLogging(work, log);
 
-work(1); // 1, добавлено в log
-work(5); // 5, добавлено в log
+// work(1); // 1, добавлено в log
+// work(5); // 5, добавлено в log
 
-for (var i = 0; i < log.length; i++) {
-  console.log( 'Лог:' + log[i] ); // "Лог:1", затем "Лог:5"
-}
+// for (var i = 0; i < log.length; i++) {
+//   console.log( 'Лог:' + log[i] ); // "Лог:1", затем "Лог:5"
+// }
+
+// Task 2 Разобрали на уроке (Не понял принципиального отличия от предыдущей задачи. Можно изначально написать не work(a), а work(a, b). А если будет передано больше параметров, чем 2, то их же интрукция работы этой функции а+b не сработает. Не понимаю в чем сложность задачи.)
+
+// Task 3 Не доделал. Потом разобрали на уроке. 
+// function f(x) {
+//   return Math.random() * x;
+// }
+
+// function makeCaching(f) {
+//   let result;
+//   return function (n) {
+//     if (n !== result) {
+//       result = f.apply(this, arguments);
+//       return result;
+//     }
+//     return result;
+//   }
+
+// }
+
+// f = makeCaching(f);
+
+// var a, b;
+
+// a = f(1);
+// console.log(a);
+// b = f(1);
+// console.log(a === b); // true (значение закешировано)
+
+// b = f(2);
+// console.log(a === b); // false, другой аргумент => другое значение
+
---
+++ public/views/javascript/homework-16/js/script.js
@@ -0,0 +1,60 @@
+'use strict';
+// Unit link - http://web.archive.org/web/20190515020906/http://learn.javascript.ru/class-instanceof
+
+// Task 1 // Решал два часа потом подсмотрел ответы.
+
+// function getSomeDate (date) {
+//   var day = date.getDate();
+//   if (day < 10) {
+//     day = '0' + day;
+//   }
+//   var month = date.getMonth() + 1;
+//   if (month < 10) {
+//     month = '0' + month;
+//   }
+//   var year = date.getFullYear() % 100;
+//   if (year < 10) {
+//     year = '0' + year;
+//   }
+//   var formattedDate = day + '.' + month + '.' + year;
+//   return formattedDate;
+// }
+
+// function formatDate (date) {
+//   if (typeof date === 'number') {
+//     date = new Date(date * 1000);
+//   } else {
+//     if (typeof date === 'string') {
+//       date = new Date(date); 
+//     } else {
+//       if (Array.isArray(date)) { 
+//         date = new Date(date[0], date[1], date[2]);
+//       }
+//     }
+//   }
+//   return getSomeDate(date);
+// }
+// console.log(formatDate('2011-10-02')); // 02.10.11
+// console.log(formatDate(1234567890)); // 14.02.09
+// console.log(formatDate([2014, 0, 1])); // 01.01.14
+// console.log(formatDate(new Date(2014, 0, 1))); // 01.01.14
+
+// Unit link - http://web.archive.org/web/20190603122521/http://learn.javascript.ru/json
+
+// Task 1
+// var leader = {
+//   name: 'Василий Иванович',
+//   age: 35
+// };
+// leader = JSON.stringify(leader);
+
+// leader = JSON.parse(leader);
+
+// Task 2
+// 1. прямой вызов JSON.stringify(team) не может превратить объект team в JSON т.к. два объекта внутри этого объекта имеют ссылки друг на друга.
+// 2. способ реализации посмотрел в ответах.
+
+// Unit link - http://web.archive.org/web/20190706120809/https://learn.javascript.ru/settimeout-setinterval
+
+// Task 1
+
