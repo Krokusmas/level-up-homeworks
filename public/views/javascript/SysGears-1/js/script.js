3fa557ff1081d702a852cf517b8bc1e206c7da06
Krokusmas <anton.sedchenko@gmail.com>
2019-11-22 23:58:40 +0200

    SysGears task1

--- public/views/javascript/SysGears-1/js/script.js
+++ public/views/javascript/SysGears-1/js/script.js
@@ -16,50 +16,55 @@
 // 2. Добавить пробелы в 5 / 9.
 // 3. Убрать запятые после if.
 // 4. Создать костанты.
+// 5. param.length положить в константу
+// 6. при любом занчении делаю парсфлоат(парам). это можно сделать один раз.
 
 // To do
 // 1. Добавить проверки на неверный формат
-// 2. param.length оложить в константу
-// 3. при любом занчении делаю парсфлоат(парам). это можно сделать один раз.
 // 4. Описать всю логику через класс TemperatureConverter с основным методом convert()
 
-function temperatureConverter (param) {
-  let celsius = 0;
-  let fahrenheit = 0;
-  let kelvin = 0;
-  let result = {};
-  const kelvinConst = 273.15;
-  const fahrenheitConst = 32;
-  param += '';
-  if (param.charAt(param.length - 1) === 'C') { // Блок для Цельсия 
-    celsius = parseFloat(param);
-    fahrenheit = Math.round(9 / 5 * celsius + fahrenheitConst);
-    kelvin = Math.round(celsius + kelvinConst);
-    result = {
-      K: kelvin,
-      F: fahrenheit
-    };
+let temperatureConverter = class TemperatureConverter {
+  convert (param) {
+    let celsius = 0;
+    let fahrenheit = 0;
+    let kelvin = 0;
+    let result = {};
+    const kelvinConst = 273.15;
+    const fahrenheitConst = 32;
+    param += '';
+    let temperatureScale = param.charAt(param.length - 1);
+    let value = parseFloat(param);
+    if (temperatureScale === 'C') { // Блок для Цельсия 
+      celsius = value;
+      fahrenheit = Math.round(9 / 5 * celsius + fahrenheitConst);
+      kelvin = Math.round(celsius + kelvinConst);
+      result = {
+        K: kelvin,
+        F: fahrenheit
+      };
+    }
+    if (temperatureScale === 'K') { // Блок для Кельвина
+      kelvin = value;
+      celsius = Math.round(kelvin - kelvinConst);
+      fahrenheit = Math.round(9 / 5 * (kelvin - kelvinConst) + fahrenheitConst);
+      result = {
+        C: celsius,
+        F: fahrenheit
+      };
+    }
+    if (temperatureScale === 'F') { // Блок для Фаренгейта
+      fahrenheit = value;
+      celsius = Math.round(5 / 9 * (fahrenheit - fahrenheitConst));
+      kelvin = Math.round((fahrenheit - fahrenheitConst) * (5 / 9) + kelvinConst);
+      result = {
+        C: celsius,
+        K: kelvin
+      };
+    }
+    let str = JSON.stringify(result);
+    console.log(str);
+    return str;
   }
-  if (param.charAt(param.length - 1) === 'K') { // Блок для Кельвина
-    kelvin = parseFloat(param);
-    celsius = Math.round(kelvin - kelvinConst);
-    fahrenheit = Math.round(9 / 5 * (kelvin - kelvinConst) + fahrenheitConst);
-    result = {
-      C: celsius,
-      F: fahrenheit
-    };
-  }
-  if (param.charAt(param.length - 1) === 'F') { // Блок для Фаренгейта
-    fahrenheit = parseFloat(param);
-    celsius = Math.round(5 / 9 * (fahrenheit - fahrenheitConst));
-    kelvin = Math.round((fahrenheit - fahrenheitConst) * (5 / 9) + kelvinConst);
-    result = {
-      C: celsius,
-      K: kelvin
-    };
-  }
-  let str = JSON.stringify(result);
-  console.log(str);
-  return str;
 }
-temperatureConverter('72C');
+
+new temperatureConverter().convert('72C');
