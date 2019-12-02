8cea8e5836c5bb4493f4316b1f8feb108d23aaed
Krokusmas <anton.sedchenko@gmail.com>
2019-12-02 18:35:12 +0200

    bug fixes

--- public/views/javascript/SysGears-1/js/script.js
+++ public/views/javascript/SysGears-1/js/script.js
@@ -16,55 +16,67 @@
 // 2. Добавить пробелы в 5 / 9.
 // 3. Убрать запятые после if.
 // 4. Создать костанты.
-// 5. param.length положить в константу
+// 5. temperature.length положить в константу
 // 6. при любом занчении делаю парсфлоат(парам). это можно сделать один раз.
+// 7. Описать всю логику через класс TemperatureConverter с основным методом convert()
+// 1) Добавил три метода внутри основного метода onvert, но выдается ошибка, что там неуместна закрывающая скобка метода. Не пойму в чем косяк.
+// 2) Добавил статик поля с константами и зменил onst в названии на kelvinNumber и т.д.
+// 3) Входной параметр изменил с param на temperature.
+// 4) такая запись зачем нужна? let temperatureConverter = class TemperatureConverter ...... Я не знаю. Мы типа сохраняем в переменную код функции для удобства вызова, но я сам не понимаю, если конструктор имеет такое же название только с заглавной и вызвать его можно так же с new.
 
 // To do
 // 1. Добавить проверки на неверный формат
-// 4. Описать всю логику через класс TemperatureConverter с основным методом convert()
 
 let temperatureConverter = class TemperatureConverter {
-  convert (param) {
+  static kelvinNumber = 273.15;
+  static fahrenheitNumber = 32;
+
+  convert (temperature) {
     let celsius = 0;
     let fahrenheit = 0;
     let kelvin = 0;
     let result = {};
-    const kelvinConst = 273.15;
-    const fahrenheitConst = 32;
-    param += '';
-    let temperatureScale = param.charAt(param.length - 1);
-    let value = parseFloat(param);
-    if (temperatureScale === 'C') { // Блок для Цельсия 
-      celsius = value;
-      fahrenheit = Math.round(9 / 5 * celsius + fahrenheitConst);
-      kelvin = Math.round(celsius + kelvinConst);
-      result = {
-        K: kelvin,
-        F: fahrenheit
-      };
-    }
-    if (temperatureScale === 'K') { // Блок для Кельвина
-      kelvin = value;
-      celsius = Math.round(kelvin - kelvinConst);
-      fahrenheit = Math.round(9 / 5 * (kelvin - kelvinConst) + fahrenheitConst);
-      result = {
-        C: celsius,
-        F: fahrenheit
-      };
-    }
-    if (temperatureScale === 'F') { // Блок для Фаренгейта
-      fahrenheit = value;
-      celsius = Math.round(5 / 9 * (fahrenheit - fahrenheitConst));
-      kelvin = Math.round((fahrenheit - fahrenheitConst) * (5 / 9) + kelvinConst);
-      result = {
-        C: celsius,
-        K: kelvin
-      };
-    }
+    let temperatureScale = temperature.charAt(temperature.length - 1);
+    let value = parseFloat(temperature);
+
+      fromCelsium () {
+        if (temperatureScale === 'C') { // Блок для Цельсия 
+          celsius = value;
+          fahrenheit = Math.round(9 / 5 * celsius + fahrenheitNumber);
+          kelvin = Math.round(celsius + kelvinNumber);
+          result = {
+            K: kelvin,
+            F: fahrenheit
+          };
+        }
+      }
+      fromKelvin () {
+        if (temperatureScale === 'K') { // Блок для Кельвина
+          kelvin = value;
+          celsius = Math.round(kelvin - kelvinNumber);
+          fahrenheit = Math.round(9 / 5 * (kelvin - kelvinNumber) + fahrenheitNumber);
+          result = {
+            C: celsius,
+            F: fahrenheit
+          };
+        }
+      }
+      fromFahrenheit () {
+        if (temperatureScale === 'F') { // Блок для Фаренгейта
+          fahrenheit = value;
+          celsius = Math.round(5 / 9 * (fahrenheit - fahrenheitNumber));
+          kelvin = Math.round((fahrenheit - fahrenheitNumber) * (5 / 9) + kelvinNumber);
+          result = {
+            C: celsius,
+            K: kelvin
+          };
+        }
+      }
+
     let str = JSON.stringify(result);
     console.log(str);
     return str;
   }
 }
 
-new temperatureConverter().convert('72C');
+new temperatureConverter().convert('72C').fromCelsium();
